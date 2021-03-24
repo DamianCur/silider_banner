b3768a3d96d6c47c67fec635e2a630c5d2fa2c8b
Damian Cur <damian.cur91@gmail.com>
2021-03-24 20:09:18 +0100

    slide changing on arrow click

--- main.js
+++ main.js
@@ -1,23 +1,46 @@
+// 1. Klawisz <- (stzałka w lewo) przesuwa w lewo (cofa) slider
+// 2. Klawisz -> (stzałka w lewo) przesuwa w prawo slider (do przod, czyli tak jak przy funkcji changeSlide) 
+// lewa strzałka: keyCode = 37
+// prawy strzałka: keyCode = 39
+// 3. (strzałki) wstrzymuje setInterval, a po zmianie slajdu uruchamiają go ponownie (czas ma się liczyć ponownie)
+
+// Projekt tutaj (przetestuj działajanie strzałek na klawiaturze)
+// https://websamuraj.pl/examples/js/projekt9/
+
+
+
 const slideList = [{
     img: "images/img1.jpg",
-    text: "Pierwszy tekst"
+    text: "Pierwszy tekst",
 }, {
     img: "images/img2.jpg",
-    text: "Drugi tekst"
+    text: "Drugi tekst",
 }, {
     img: "images/img3.jpg",
-    text: "Trzeci tekst"
+    text: "Trzeci tekst",
+
 }];
 
 
 
 const image = document.querySelector("img");
 const h1 = document.querySelector("h1");
+const dots = [...document.querySelectorAll(".dots span")]
 //INTERFEJS
 const time = 5000;
 let active = 0;
 
 
+
+
+//IMPLEMENTACJA
+
+const changeDot = () => {
+    const activeDot = dots.findIndex(dot => dot.classList.contains("active"));
+
+    dots[activeDot].classList.remove("active");
+    dots[active].classList.add("active");
+}
 
 const changeSlide = () => {
     active++
@@ -26,7 +49,33 @@ const changeSlide = () => {
     }
     image.src = slideList[active].img;
     h1.textContent = slideList[active].text
+    changeDot()
+}
+let slideInterval = setInterval(changeSlide, time);
+
+
+// utwórz funkcje keyChangeSlide. Zadanie może wymagać także zmian poza funkcją.
+const keyChangeSlide = (e) => {
+
+    if (e.keyCode === 37 || e.keyCode === 39) {
+        clearInterval(slideInterval);
+        e.keyCode === 37 ? active-- : active++;
+        if (active >= slideList.length) {
+            active = 0;
+        } else if (active <= -1) {
+            active = slideList.length - 1;
+        }
+
+        image.src = slideList[active].img;
+        h1.textContent = slideList[active].text
+        changeDot()
+        slideInterval = setInterval(changeSlide, time);
+    }
+
 
 }
-setInterval(changeSlide, time);
-//IMPLEMENTACJA
+
+
+
+
+window.addEventListener('keydown', keyChangeSlide)
